22ed622db356da95a8294bdcc2a59427dfa128a5
pooja panwar <poojapanwar@beeonline.co.in>
2026-03-02 17:01:04 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+﻿import React from "react";
 import "../app/globals.css";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
@@ -6,6 +6,9 @@ export const metadata = {
   title: "Shree Balaji Agro | Agricultural Equipment Manufacturer Since 1975",
   description:
     "Shree Balaji Agro, established in 1975, is a trusted manufacturer of agricultural equipment including cultivators, seed drills, rotavators, and farm implements. Quality machinery for modern farming.",
+  icons: {
+    icon: "/icon.webp",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -22,3 +25,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
